b4130af8bb094ef771b7c6201259f25f594c6e81
Yuichiro Someya <yuichiro-someya@cookpad.com>
2017-06-25 11:23:29 +0900

    Fix csv problems

--- akagi/iterators/csv_iterator.py
+++ akagi/iterators/csv_iterator.py
@@ -4,12 +4,14 @@ import six
 
 
 class CSVIterator(object):
-    def __init__(self, content):
+    def __init__(self, content, skip_errors=True):
         self.content = content
+        self._skip_erros = skip_errors
+        self._iterator = None
 
     @classmethod
     def open_file(cls, path):
-        return open(path, 'r')
+        return open(path, newline='')
 
     @classmethod
     def decode(cls, content):
@@ -18,5 +20,18 @@ class CSVIterator(object):
         else:
             return content.decode('utf-8')
 
+    def __next__(self):
+        try:
+            n = self._iterator.__next__()
+            return n
+        except StopIteration as e:
+            raise e
+        except Exception as e:
+            if self._skip_errors:
+                return self._iterator.__next__()
+            else:
+                raise e
+
     def __iter__(self):
-        return csv.reader(StringIO(self.content))
+        self._iterator = csv.reader(StringIO(self.content), escapechar='\\')
+        return self
