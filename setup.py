b5ba5f4ec146711b0111fa4d99f125d79e4ac558
Yuichiro Someya <yuichiro-someya@cookpad.com>
2017-12-25 18:05:21 +0900

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='akagi',
-    version='0.2.1',
+    version='0.2.2',
     description="Codenize your data sources",
     long_description=readme,
     author="Yuichiro Someya",
